1b9a48821b98dbfd3fa6d8005d334a7ea2b8dfca
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-27 23:43:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 }; 
 
  function updatePlaylist(playlist,artistname,songtitle) {
-  playlist['Demi Lovato]
+  playlist[Demi Lovato]
 };
 object.assign ({}, playlist,{'Demi Lovato':['Get Back']})
 
