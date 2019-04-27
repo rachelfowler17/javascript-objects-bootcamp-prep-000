91c1c163d57c70fb8db8f2f4f70a7389e3f14b62
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-27 22:38:51 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,20 @@
+var playlist = {
+  Beyonce: "Sorry",
+  Michael Jackson: "Beat it",
+  Celine Dion: "All Night"
+}; 
+
+var updatePlaylist = {
+  Beyonce: "Sorry",
+  Michael Jackson: "Beat it",
+  Celine Dion: "All Night"
+};
+object.assign ({}, playlist,{'Demi Lovato':['Get Back']})
+
+var removeFromPlaylist = {
+  Beyonce: "Sorry",
+  Michael Jackson: "Beat it",
+  Celine Dion: "All Night"
+};
+delete Celine Dion.All Night; 
+playlist; 
